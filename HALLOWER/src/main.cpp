7db26b008b9ec65de61a0d77083a6d5efc496872
Alden Wyatt <aawyatt04@gmail.com>
2026-07-16 15:54:48 -0700

    fixed game dimensions and made the window scalable using a render texture

--- HALLOWER/src/main.cpp
+++ HALLOWER/src/main.cpp
@@ -6,9 +6,15 @@
 
 int main()
 {
-    // Testing github commit
-    InitWindow(640, 640, "GAME");
+    // the diminsions of the window in pixels
+    Vector2 windowSize = {256, 240};
+    // how much the window is scaled by
+    int windowScale = 4;
+    // the window is intitatied with the dimisions of the windowSize * the window scale
+    InitWindow((windowSize.x * windowScale), (windowSize.y * windowScale), "GAME");
     SetTargetFPS(60);
+    // RenderTexture that is drawen to and scaled up to fit the screen
+    RenderTexture2D gameRender = LoadRenderTexture(windowSize.x, windowSize.y);
 
     Player player;
     Vector2 spriteSize = {24, 24};
@@ -27,13 +33,26 @@ int main()
         {
             player.Colliding();
         }
-        BeginDrawing();
+        // draw everything to the gameRender texture
+        BeginTextureMode(gameRender);
 
         ClearBackground(BLUE);
-
         DrawRectangleRec(wall, GRAY);
         player.Update();
         sprite.Update();
+
+        EndTextureMode();
+
+        // in the drawing mode draw the gameRender texture
+        BeginDrawing();
+        /*DrawTexturePro(texture, source, dest, rotation, color), the source is equal to the pixel dimensions defined earlier,
+        and the dest is equal to actual window dimensions, scaling the gameRender up*/
+        DrawTexturePro(gameRender.texture,
+                       Rectangle{0.0f, 0.0f, (float)gameRender.texture.width, (float)-gameRender.texture.height},
+                       Rectangle{0.0f, 0.0f, (float)GetScreenWidth(), (float)GetScreenHeight()},
+                       Vector2{0, 0},
+                       0.0f,
+                       WHITE);
 
         EndDrawing();
     }
